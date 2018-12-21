469e936bc0a75f6219485b88c68e912cb8a5bb83
Winter Wilson <winter.sunshine.wilson@gmail.com>
2018-12-21 02:45:45 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ def speak_to_grandma(phrase)
  
 if phrase == "I LOVE YOU GRANDMA"
   return "I LOVE YOU TOO PUMPKIN."
-elsif phrase == "phrase.downcase"
+elsif phrase == phrase.downcase
   return "NO, NOT SINCE 1938!"
 else
   return "HUH?! SPEAK UP, SONNY!"
